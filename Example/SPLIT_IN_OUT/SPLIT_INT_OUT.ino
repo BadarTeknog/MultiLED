a2afa33095a0b279ee2246418d8ce38d02658048
Badar Teknog <82695671+BadarTeknog@users.noreply.github.com>
2021-07-11 20:22:32 -1200

    Add files via upload

---
+++ Example/SPLIT_IN_OUT/SPLIT_INT_OUT.ino
@@ -0,0 +1,21 @@
+#include <MultiLED.h> //include library
+#define DLY 200 //delay 200 ms
+/*
+=============================================
+MultiLED by "Badar Teknog"
+Dowbload now library
+https://github.com/BadarTeknog/MultiLED
+=============================================
+*/
+MultiLED led;
+void setup() { 
+    led.OUT_Array(2,10);
+}
+void loop() {
+led.SPLIT_INT(2,10,DLY);
+led.Clear();
+delay(DLY);    
+led.SPLIT_OUT(2,10,DLY);
+led.Clear();
+delay(DLY);
+}
